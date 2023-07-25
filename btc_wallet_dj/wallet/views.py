e63e009df0f6b1fee10d771ae7d4982e81004f6d
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-07-25 15:53:10 +0600

    register and login page

--- btc_wallet_dj/wallet/views.py
+++ btc_wallet_dj/wallet/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.htm')
+
+def login(request):
+    return render(request, 'login.htm')
+
+def register(request):
+    return render(request, 'register.htm')
